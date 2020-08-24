ffe9b8c8e63f0d97b4c8e6db2689fe3f89211ddf
Ilya Zaytsev <ilya@zaytsev.xyz>
2020-08-24 22:25:09 +0300

    strategy pattern

---
+++ Strategy/Duck.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace Strategy
+{
+    public abstract class Duck
+    {
+        public IFlyBehavior FlyBehavior { get; set; }
+        public IQuackBehavior QuackBehavior { get; set; }
+
+        public Duck() { }
+
+        public abstract void Display();
+
+        public void PerformFly()
+        {
+            FlyBehavior.ToFly();
+        }
+
+        public void PerformQuack()
+        {
+            QuackBehavior.ToQuack();
+        }
+
+        public void Swim()
+        {
+            Console.WriteLine("All ducks float, even decoys!");
+        }
+    }
+
+    public class MallardDuck : Duck
+    {
+        public MallardDuck()
+        {
+            QuackBehavior = new Quack();
+            FlyBehavior = new FlyWithWings();
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine("I'm a real Mallard duck");
+        }
+    }
+
+    public class ModelDuck : Duck
+    {
+        public ModelDuck()
+        {
+            FlyBehavior = new FlyNoWay(); // Утка-приманка изначально летать не умеет...
+            QuackBehavior = new Quack();
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine("I'm a model duck");
+        }
+    }
+
+    public interface IFlyBehavior // Интерфейс реализуется всеми классами
+    {
+        void ToFly();
+    }
+
+    public class FlyWithWings : IFlyBehavior // Реализация поведения для уток, которые умеют летать
+    {
+        public void ToFly()
+        {
+            Console.WriteLine("I am flying!");
+        }
+    }
+
+    public class FlyNoWay : IFlyBehavior // Реализация поведения для утрок, которые не летают
+    {
+        public void ToFly()
+        {
+            Console.WriteLine("I can't fly :(");
+        }
+    }
+
+    public class FlyRocketPowered : IFlyBehavior
+    {
+        public void ToFly()
+        {
+            Console.WriteLine("I'm flying with a rocket!");
+        }
+    }
+
+    public interface IQuackBehavior
+    {
+        void ToQuack();
+    }
+
+    public class Quack : IQuackBehavior
+    {
+        public void ToQuack()
+        {
+            Console.WriteLine("Quack");
+        }
+    }
+
+    public class MuteQuack : IQuackBehavior
+    {
+        public void ToQuack()
+        {
+            Console.WriteLine("<< Silence >>");
+        }
+    }
+
+    public class Squeak : IQuackBehavior
+    {
+        public void ToQuack()
+        {
+            Console.WriteLine("Squeak");
+        }
+    }
+}
---
+++ Strategy/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Strategy
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Паттерн Стратегия!
+
+            Duck mallard = new MallardDuck();
+
+            // Вызов метода performQuack(), унаследованного классом MallardDuck;
+            // метод делегирует выполнение операции по ссылке на QuackBehavior
+            // (то есть вызывает quack() через унаследованную переменную quackBehavior)
+            mallard.PerformQuack();
+            // Затем то же самое происходит с методом performFly(), также унаследованным классом MallardDuck
+            mallard.PerformFly();
+
+            Duck model = new ModelDuck();
+            
+            // Первый вызов performFly() передаётся реализации, заданной в конструкторе...
+            // ...ModelDuck - то есть экземпляру FlyNoWay
+            model.PerformFly();
+            // Утка-приманка вдруг взлетает на рекативном двигателе!
+            model.FlyBehavior = new FlyRocketPowered();
+            // Способность утки-приманки к полёту переключается динамически!
+            // Если бы реализация находилась в иерархии Duck, такое было бы невозможно
+            model.PerformFly();
+
+            Console.Read();
+        }
+    }
+}
